f11fc8b260db60fe928dd7bb16c8261fb6b355c1
jd <duverseaujohnny21@gmail.com>
2020-06-26 16:09:17 -0400

    test the first spec

--- __tests__/galactic_age_calculator.tests.js
+++ __tests__/galactic_age_calculator.tests.js
@@ -1,2 +1,21 @@
 // Test logic!
-// Probably want you import something you're using in the project-name.js to test it!
+import { Planets } from './../src/galactic_age_calculator.js';
+
+describe('Planets', () => {
+
+  let calculateEarth;
+ 
+  beforeEach( () => {
+    calculateEarth = new Planets(.24, .62, 1.88, 11.86, 365);
+  })
+
+  test("A solar year lasts 365 days on planet Earth, a Mercury year is .24 Earth years, a Venus year is .62 Earth years, a Mars year is 1.88 Earth years, (A Jupiter year is 11.86 Earth years.", () => {
+      expect(calculateEarth.mercury).toBeCloseTo(.24);
+      expect(calculateEarth.venus).toBeCloseTo(.62);
+      expect(calculateEarth.mars).toBeCloseTo(1.88);
+      expect(calculateEarth.jupiter).toBeCloseTo(11.88);
+      expect(calculateEarth.earth).toBe(1);
+  });
+
+
+});

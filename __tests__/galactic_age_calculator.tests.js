d0397d189d45c70944a8489ae7b36f278efe3a94
jd <duverseaujohnny21@gmail.com>
2020-06-26 19:09:15 -0400

    test cause i don't have  corresponding

--- __tests__/galactic_age_calculator.tests.js
+++ __tests__/galactic_age_calculator.tests.js
@@ -3,11 +3,6 @@ import { Planets } from './../src/galactic_age_calculator.js';
 
 describe('Planets', () => {
 
-  // let calculateEarth;
- 
-  // beforeEach( () => {
-  //   calculateEarth = new Planets(0.24, 0.62, 1.88, 11.86, 1);
-  // })
 
   // test("A solar year lasts 1 year on planet Earth, a Mercury year is .24 Earth years, a Venus year is .62 Earth years, a Mars year is 1.88 Earth years, (A Jupiter year is 11.86 Earth years.", () => {
   //     expect(calculateEarth.mercury).toBeCloseTo(0.24);
@@ -16,10 +11,15 @@ describe('Planets', () => {
   //     expect(calculateEarth.jupiter).toBeCloseTo(11.88);
   //     expect(calculateEarth.earth).toBeCloseTo(1);
   // });
-  test("If a user is 38 years old on Earth, their age on Mercury is 0.24 Earth years and 38 /(0.24) = 158 years old ", () => {
+  test("If a user is 38 years old on Earth, their age on Mercury is .24 Earth years and 38 /(.24) = 158 years old ", () => {
      let calculateMercury = new Planets(.24, .62, 1.88, 11.88, 0);
      calculateMercury.checkMercury();
     expect(calculateMercury.earth).toBe(158.33333333333334);
+   })
+   test("If a user is 60 years old on Earth, their age on Venus is .62 Earth years and 60 /(.62) = 97 years old", () => {
+     let calculateVenus = new Planets(.24, .62, 1.88, 11.88, 11.86);
+     calculateVenus.checkMercury();
+     expect(calculateVenus.earth).toBe(96.7741935);
    })
 
 });

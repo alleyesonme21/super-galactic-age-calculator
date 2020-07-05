9536978e015d31d8271aa9e4f740147cc57e6bd6
jd <duverseaujohnny21@gmail.com>
2020-07-05 15:44:36 -0400

    add beforeEach(), parseInt() and toFIxed() to convert to number

--- __tests__/galactic_age_calculator.tests.js
+++ __tests__/galactic_age_calculator.tests.js
@@ -2,29 +2,29 @@
 import { Planets } from './../src/galactic_age_calculator.js';
 
 describe('Planets', () => {
+  let calculateAge;
 
-  test("If a user is 38 years old on Earth, their age on Mercury is .24 Earth years and 38 /(.24) = 158 years old ", () => {
-     let calculateMercury = new Planets(.24, .62, 1.88, 11.88, 0);
-     calculateMercury.checkMercury();
-    expect(calculateMercury.earth).toBe(158.33333333333334);
-   })
+beforeEach(() => {
+  calculateAge = new Planets(0.24, 0.62, 1.88, 11.88);
+})
 
-   test("If a user is 60 years old on Earth, their age on Venus is .62 Earth years and 60 /(.62) = 97 years old", () => {
-     let calculateVenus = new Planets(.24, .62, 1.88, 11.88);
-     calculateVenus.checkVenus();
-     expect(calculateVenus.earth).toBe(96.7741935483871);
-   })
-     
-   test("If a user is 20 years old on Earth, their age on Mars is 1.88 Earth years and 20 /(1.88) = 11 years old", () => {
-    let calculateMars = new Planets(.24, .62, 1.88, 11.88);
-    calculateMars.checkMars();
-    expect(calculateMars.earth).toBe(10.638297872340425);
-   })
-    test("If a user is 70 years old on Earth, their age on Jupiter is 11.86 Earth years and 70 /(11.86) = 6 years old", () => {
-      let calculateJupiter = new Planets(.24, .62, 1.88, 11.86);
-      calculateJupiter.checkJupiter();
-       expect(calculateJupiter.earth).toBe(5.902192242833053);
+test("If a user is 38 years old on Earth, their age on Mercury is .24 Earth years and 38 /(.24) = 158 years old ", () => {
+  calculateAge.checkMercury();
+  expect(parseInt(calculateAge.age.toFixed())).toBe(158);
+})
 
-    })
+test("If a user is 60 years old on Earth, their age on Venus is .62 Earth years and 60 /(.62) = 97 years old", () => {
+  calculateAge.checkVenus();
+  expect(parseInt(calculateAge.age.toFixed())).toBe(97);
+})
 
+test("If a user is 20 years old on Earth, their age on Mars is 1.88 Earth years and 20 /(1.88) = 11 years old", () => {
+  calculateAge.checkMars();
+  expect(parseInt(calculateAge.age.toFixed())).toBe(11);
+})
+
+test("If a user is 70 years old on Earth, their age on Jupiter is 11.86 Earth years and 70 /(11.86) = 6 years old", () => {
+  calculateAge.checkJupiter();
+  expect(parseInt(calculateAge.age.toFixed())).toBe(6);
+});
 });

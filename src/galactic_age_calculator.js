f478b2e731a18ddd3d1fe49c30d64232646d93bb
jd <duverseaujohnny21@gmail.com>
2020-07-08 10:33:19 -0400

    refactor, add Math.roun()and indent

--- src/galactic_age_calculator.js
+++ src/galactic_age_calculator.js
@@ -1,43 +1,42 @@
 //Business Logic
 export class Planets { 
 
-constructor(age) {
+constructor(age, lifeExpectancy) {
   this.age = age;
+  this.lifeExpectancy = lifeExpectancy;
 }
+
 checkMercury() {
-  this.age = 38;
-  this.age /= 0.24; 
+  this.age = parseInt((this.age /= 0.24).toFixed()); 
 }
 checkVenus() {
-  this.age = 60;
-  this.age /= 0.62;
+  this.age = parseInt((this.age /= 0.62).toFixed()); 
 }
 checkMars() {
-  this.age = 20;
-  this.age /= 1.88;
+  this.age = parseInt((this.age /= 1.88).toFixed()); 
 }
 checkJupiter() {
-  this.age = 70;
-  this.age /= 11.86;
+  this.age = parseInt((this.age /= 11.86).toFixed()); 
 }
+
 expectancyMercury() {
-  this.age = 300;
-  this.age -= 200; 
+  this.checkMercury();
+  const mercuryExpectancy = Math.round((this.lifeExpectancy/0.24) - this.age);
+  return `You have ${mercuryExpectancy} left to live`;
 }
 expectancyVenus() {
-  this.age = 100;
-  this.age -= 70; 
-}
-expectancyEarth() {
-  this.age = 80;
-  this.age -= 60; 
+  this.checkVenus();
+  const venusExpectancy = Math.round((this.lifeExpectancy/0.62) - this.age);
+  return `You have ${venusExpectancy} left to live`;
 }
 expectancyMars() {
-  this.age = 60;
-  this.age -= 50; 
+  this.checkMars();
+  const marsExpectancy = Math.round((this.lifeExpectancy/1.88) - this.age);
+  return `You have ${marsExpectancy} left to live`;
 }
 expectancyJupiter() {
-  this.age = 30;
-  this.age -= 25; 
+  this.checkJupiter();
+  const jupiterExpectancy = Math.round((this.lifeExpectancy/11.86) - this.age);
+  return `You have ${jupiterExpectancy} left to live`;
 }
 };

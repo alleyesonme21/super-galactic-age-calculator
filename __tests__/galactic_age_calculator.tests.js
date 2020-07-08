fe76a1034db97fbf27f097005534633b0abee01a
jd <duverseaujohnny21@gmail.com>
2020-07-08 10:08:33 -0400

    test failed

--- __tests__/galactic_age_calculator.tests.js
+++ __tests__/galactic_age_calculator.tests.js
@@ -3,58 +3,49 @@ import { Planets } from './../src/galactic_age_calculator.js';
 
 describe('Planets', () => {
   let calculateAge;
-  let calculateExpect;
-
-  beforeEach(() => {
-  calculateAge = new Planets(0.24, 0.62, 1.88, 11.88);
-})
-
-test("If a user is 38 years old on Earth, their age on Mercury is .24 Earth years and 38 /(.24) = 158 years old ", () => {
-  calculateAge.checkMercury();
-  expect(parseInt(calculateAge.age.toFixed())).toBe(158);
-})
-
-test("If a user is 60 years old on Earth, their age on Venus is .62 Earth years and 60 /(.62) = 97 years old", () => {
-  calculateAge.checkVenus();
-  expect(parseInt(calculateAge.age.toFixed())).toBe(97);
-})
-
-test("If a user is 20 years old on Earth, their age on Mars is 1.88 Earth years and 20 /(1.88) = 11 years old", () => {
-  calculateAge.checkMars();
-  expect(parseInt(calculateAge.age.toFixed())).toBe(11);
-})
-
-test("If a user is 70 years old on Earth, their age on Jupiter is 11.86 Earth years and 70 /(11.86) = 6 years old", () => {
-  calculateAge.checkJupiter();
-  expect(parseInt(calculateAge.age.toFixed())).toBe(6);
-});
 
 beforeEach(() => {
-  calculateExpect = new Planets(300, 100, 80, 60, 30);
+  calculateAge = new Planets(38, 90);
 })
 
-test("The life expectancy is 300 years on Mercury and users's age is 200 years, the years left to live is 300 - 200 user's age = 100 ", () => {
-  calculateExpect.expectancyMercury();
-expect(calculateExpect.age).toBe(100)
-})
+test("It will show the users current  age on Mercury. ", () => {
+  calculateAge.checkMercury();
+  expect(calculateAge.age).toBe(158);
+});
 
-test("The life expectancy is 100 years on Venus and users's age is 70 years, the years left to live is 100 - 70 user's age = 30 ", () => {
-  calculateExpect.expectancyVenus();
-  expect(calculateExpect.age).toBe(30)
-})
+test("It will show the users current age on venus.", () => {
+  calculateAge.checkVenus();
+  expect(calculateAge.age).toBe(61);
+});
 
-test("The life expectancy is 80 years on Earth and users's age is 60 years, the years left to live is 80 - 60 user's age = 20 ", () => {
-  calculateExpect.expectancyEarth();
-  expect(calculateExpect.age).toBe(20)
-})
+test("It will show the users current age on Mars.", () => {
+  calculateAge.checkMars();
+  expect(calculateAge.age).toBe(20);
+});
 
-test("The life expectancy is 60 years on Mars and users's age is 50 years, the years left to live is 60 - 50 user's age = 10 ", () => {
-  calculateExpect.expectancyMars();
-  expect(calculateExpect.age).toBe(10)
-})
+test("It will show the users current age on Jupiter.", () => {
+  calculateAge.checkJupiter();
+  expect(calculateAge.age).toBe(3);
+});
 
-test("The life expectancy is 30 years on Jupiter and users's age is 25 years, the years left to live is 30 - 25  = 5 years", () => {
-  calculateExpect.expectancyJupiter();
-  expect(calculateExpect.age).toBe(5)
-})
-}); 
+test("It will show the users'years left to live on Mercury", () => {
+  // calculateExpect.expectancyVenus();
+  expect(calculateAge.expectancyMercury()).toBe()
+});
+
+test("It will show the users'years left to live on venus", () => {
+  // calculateExpect.expectancyVenus();
+  expect(calculate.expectancyVenus()).toBe()
+});
+
+ test("It will show the users'years left to live on Mars", () => {
+  //  calculateExpect.expectancyMars();
+   expect(calculateExpect.expectancyMars()).toBe()
+ });
+
+test("It will show the users'years left to live on Jupiter", () => {
+  // calculateAge.expectancyJupiter();
+  expect(calculateAge.expectancyJupiter()).toBe("You have 5 left to live")
+});
+
+});

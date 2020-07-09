5f70990c754916d7d8879537403df1161375c4fd
jd <duverseaujohnny21@gmail.com>
2020-07-09 01:00:33 -0400

    refactor, indented and all pass

--- __tests__/galactic_age_calculator.tests.js
+++ __tests__/galactic_age_calculator.tests.js
@@ -1,47 +1,46 @@
-// Test logic!
+// Business logic!
 import { Planets } from './../src/galactic_age_calculator.js';
 
 describe('Planets', () => {
-  let calculateAge;
+  let calculateAge; 
 
-beforeEach(() => {
-  calculateAge = new Planets(38, 90);
-})
+  beforeEach(() => {
+    calculateAge = new Planets(70, 95);
+  })
 
-test("It will show the users current  age on Mercury. ", () => {
-  calculateAge.checkMercury();
-  expect(calculateAge.age).toBe(158);
-});
+  test("It will show the users current  age on Mercury. ", () => {
+    calculateAge.checkMercury();
+    expect(calculateAge.age).toBe(292);
+  });
 
-test("It will show the users current age on venus.", () => {
-  calculateAge.checkVenus();
-  expect(calculateAge.age).toBe(61);
-});
+  test("It will show the users current age on venus.", () => {
+    calculateAge.checkVenus();
+    expect(calculateAge.age).toBe(113);
+  });
 
-test("It will show the users current age on Mars.", () => {
-  calculateAge.checkMars();
-  expect(calculateAge.age).toBe(20);
-});
+  test("It will show the users current age on Mars.", () => {
+    calculateAge.checkMars();
+    expect(calculateAge.age).toBe(37);
+  });
 
-test("It will show the users current age on Jupiter.", () => {
-  calculateAge.checkJupiter();
-  expect(calculateAge.age).toBe(3);
-});
+  test("It will show the users current age on Jupiter.", () => {
+    calculateAge.checkJupiter();
+    expect(calculateAge.age).toBe(6);
+  });
 
-test("It will show the users'years left to live on Mercury", () => {
-  expect(calculateAge.expectancyMercury()).toBe("You have 217 left to live")
-});
+  test("It will show the users'years left to live on Mercury", () => {
+    expect(calculateAge.expectancyMercury()).toBe("You have lived -104 past your life expectancy")
+  });
 
-test("It will show the users'years left to live on venus", () => {
-  expect(calculateAge.expectancyVenus()).toBe("You have 84 left to live")
-});
+  test("It will show the users'years left to live on venus", () => {
+    expect(calculateAge.expectancyVenus()).toBe("You have lived -40 past your life expectancy")
+  });
 
-test("It will show the users'years left to live on Mars", () => {
-  expect(calculateAge.expectancyMars()).toBe("You have 28 left to live")
-});
+  test("It will show the users'years left to live on Mars", () => {
+    expect(calculateAge.expectancyMars()).toBe("You have lived -14 past your life expectancy")
+  });
 
-test("It will show the users'years left to live on Jupiter", () => {
-  expect(calculateAge.expectancyJupiter()).toBe("You have 5 left to live")
-});
-
+  test("It will show the users'years left to live on Jupiter", () => {
+    expect(calculateAge.expectancyJupiter()).toBe("You have lived -2 past your life expectancy")
+  });
 });

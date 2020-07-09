11230ce5df31f45b1b94b1ae0c2cc80e99ae3370
jd <duverseaujohnny21@gmail.com>
2020-07-09 01:03:01 -0400

    add branchind, refactor and indented

--- src/galactic_age_calculator.js
+++ src/galactic_age_calculator.js
@@ -1,41 +1,60 @@
 //Business Logic
 export class Planets { 
 
-constructor(age, lifeExpectancy) {
-  this.age = age;
-  this.lifeExpectancy = lifeExpectancy;
-}
-checkMercury() {
-  this.age = parseInt((this.age /= 0.24).toFixed()); 
-}
-checkVenus() {
-  this.age = parseInt((this.age /= 0.62).toFixed()); 
-}
-checkMars() {
-  this.age = parseInt((this.age /= 1.88).toFixed()); 
-}
-checkJupiter() {
-  this.age = parseInt((this.age /= 11.86).toFixed()); 
-}
+  constructor(age, lifeExpectancy) {
+    this.age = age;
+    this.lifeExpectancy = lifeExpectancy;
+  }
+  checkMercury() {
+    this.age = parseInt((this.age /= 0.24).toFixed()); 
+  }
+  checkVenus() {
+    this.age = parseInt((this.age /= 0.62).toFixed()); 
+  }
+  checkMars() {
+    this.age = parseInt((this.age /= 1.88).toFixed()); 
+  }
+  checkJupiter() {
+    this.age = parseInt((this.age /= 11.86).toFixed()); 
+  }
 
-expectancyMercury() {
-  this.checkMercury();
-  const mercuryExpectancy = Math.round((this.lifeExpectancy/0.24) - this.age);
-  return `You have ${mercuryExpectancy} left to live`;
+  expectancyMercury() {
+    this.checkMercury();
+    const mercuryExpectancy = Math.round(this.age - (this.lifeExpectancy/0.24));
+    if(mercuryExpectancy > 0) {
+      return `You have ${mercuryExpectancy} left to live`;
+    } else if(mercuryExpectancy < 0) { 
+      return `You have lived ${mercuryExpectancy} past your life expectancy`;
+    }
+  }
+
+  expectancyVenus() {
+    this.checkVenus();
+    const venusExpectancy = Math.round(this.age - (this.lifeExpectancy/0.62));
+    if(venusExpectancy > 0) {
+      return `You have ${venusExpectancy} left to live`;
+    } else if(venusExpectancy < 0) { 
+      return `You have lived ${venusExpectancy} past your life expectancy`;
+    }
+  }
+
+  expectancyMars() {
+    this.checkMars();
+    const marsExpectancy = Math.round(this.age - (this.lifeExpectancy/1.88));
+    if(marsExpectancy > 0) {
+      return `You have ${marsExpectancy} left to live`;
+    } else if(marsExpectancy < 0) { 
+      return `You have lived ${marsExpectancy} past your life expectancy`;
+    }
+  } 
+
+  expectancyJupiter() {
+    this.checkJupiter();
+    const jupiterExpectancy = Math.round(this.age - (this.lifeExpectancy/11.86));
+    if(jupiterExpectancy > 0) {
+      return `You have ${jupiterExpectancy} left to live`;
+    } else if(jupiterExpectancy < 0) { 
+      return `You have lived ${jupiterExpectancy} past your life expectancy`;
+    }
+  }
 }
-expectancyVenus() {
-  this.checkVenus();
-  const venusExpectancy = Math.round((this.lifeExpectancy/0.62) - this.age);
-  return `You have ${venusExpectancy} left to live`;
-}
-expectancyMars() {
-  this.checkMars();
-  const marsExpectancy = Math.round((this.lifeExpectancy/1.88) - this.age);
-  return `You have ${marsExpectancy} left to live`;
-}
-expectancyJupiter() {
-  this.checkJupiter();
-  const jupiterExpectancy = Math.round((this.lifeExpectancy/11.86) - this.age);
-  return `You have ${jupiterExpectancy} left to live`;
-}
-};
